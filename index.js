56d70530ab90e3b135733c41638adb78e12a6dba
“username” <emailaddr>
2022-12-26 22:31:50 +0200

    Error handler

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.status(200).send('Something broke!')
+})
+
 const Telegraf = require(`telegraf`)
 
 
