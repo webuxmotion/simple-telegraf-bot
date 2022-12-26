077a1ee0c4763e8bfd75ac2b13f895144b7a0140
“username” <emailaddr>
2022-12-26 22:59:22 +0200

    df

--- index.js
+++ index.js
@@ -1,41 +1,8 @@
 const { Telegraf } = require('telegraf');
-const { message } = require('telegraf/filters');
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
-
-bot.command('quit', async (ctx) => {
-  // Explicit usage
-  await ctx.telegram.leaveChat(ctx.message.chat.id);
-
-  // Using context shortcut
-  await ctx.leaveChat();
-});
-
-bot.on(message('text'), async (ctx) => {
-  // Explicit usage
-  await ctx.telegram.sendMessage(ctx.message.chat.id, `Hello ${ctx.state.role}`);
-
-  // Using context shortcut
-  await ctx.reply(`Hello ${ctx.state.role}`);
-});
-
-bot.on('callback_query', async (ctx) => {
-  // Explicit usage
-  await ctx.telegram.answerCbQuery(ctx.callbackQuery.id);
-
-  // Using context shortcut
-  await ctx.answerCbQuery();
-});
-
-bot.on('inline_query', async (ctx) => {
-  const result = [];
-  // Explicit usage
-  await ctx.telegram.answerInlineQuery(ctx.inlineQuery.id, result);
-
-  // Using context shortcut
-  await ctx.answerInlineQuery(result);
-});
-
+bot.command('oldschool', (ctx) => ctx.reply('Hello'));
+bot.command('hipster', Telegraf.reply('λ'));
 bot.launch();
 
 // Enable graceful stop
