04e3bceba9f2caca98532f684d0297adbaa499ae
“username” <emailaddr>
2022-12-26 23:05:40 +0200

    webhook

--- index-old.js
+++ index-old.js
@@ -24,6 +24,24 @@ bot.help((ctx) => ctx.reply(`Send me a sticker`))
 bot.on(`sticker`, (ctx) => ctx.reply(`👍`))
 bot.hears(`hi`, (ctx) => ctx.reply(`Hey there`))
 
-bot.launch()
-    .then((res) => console.log(`Launched at ${new Date()}`))
-    .catch((err) => console.log(`ERROR at launch:`, err))
+bot.launch({
+  webhook: {
+    // Public domain for webhook; e.g.: example.com
+    domain: 'https://shrouded-hamlet-06986.herokuapp.com/',
+
+    // Port to listen on; e.g.: 8080
+    port: port,
+
+    // Optional path to listen for.
+    // `bot.secretPathComponent()` will be used by default
+    //hookPath: webhookPath,
+
+    // Optional secret to be sent back in a header for security.
+    // e.g.: `crypto.randomBytes(64).toString("hex")`
+    secretToken: "sdfsdfeeee444545",
+  },
+});
+
+// bot.launch()
+//     .then((res) => console.log(`Launched at ${new Date()}`))
+//     .catch((err) => console.log(`ERROR at launch:`, err))
