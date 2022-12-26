4adb4cc27b200d2d4f825c938d08fddbc9996fb0
“username” <emailaddr>
2022-12-26 22:36:27 +0200

    DF

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000;
 
 app.use((err, req, res, next) => {
     console.error(err.stack)
