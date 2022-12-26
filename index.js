478b962aa4caaaab8f995de878db351dab074a44
“username” <emailaddr>
2022-12-26 23:16:11 +0200

    dff

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { Telegraf } = require('telegraf');
-
-const bot = new Telegraf(process.env.BOT_TOKEN);
+const { BOT_TOKEN } = process.env;
+const bot = new Telegraf(BOT_TOKEN);
 bot.command('oldschool', (ctx) => ctx.reply('Hello'));
 bot.command('hipster', Telegraf.reply('λ'));
 const port = process.env.PORT || 3000;
@@ -20,7 +20,7 @@ bot.launch({
   
       // Optional path to listen for.
       // `bot.secretPathComponent()` will be used by default
-      hookPath: 'webhook',
+      hookPath: '/webhook',
   
       // Optional secret to be sent back in a header for security.
       // e.g.: `crypto.randomBytes(64).toString("hex")`
