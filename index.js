addc40c0055f0aae5359a2a71d8d055915c26c8e
“username” <emailaddr>
2022-12-26 22:21:10 +0200

    server

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 3000
+
 const Telegraf = require(`telegraf`)
 
-const { BOT_TOKEN, URL } = process.env;
-const PORT = process.env.PORT || 80;
+
+const { BOT_TOKEN } = process.env;
 
 const bot = new Telegraf(BOT_TOKEN);
 
@@ -11,13 +15,11 @@ bot.help((ctx) => ctx.reply(`Send me a sticker`))
 bot.on(`sticker`, (ctx) => ctx.reply(`👍`))
 bot.hears(`hi`, (ctx) => ctx.reply(`Hey there`))
 
-if (process.env.NODE_ENV === 'production') {
-    bot.telegram.setWebhook(`${URL}/bot${BOT_TOKEN}`);
-    bot.startWebhook(`bot${BOT_TOKEN}'`, null, PORT);
-    console.log('Started with webhook');
-} else {
-    bot.launch()
-        .then((res) => console.log(`Launched at ${new Date()}`))
-        .catch((err) => console.log(`ERROR at launch:`, err))
-}
+bot.launch()
+    .then((res) => console.log(`Launched at ${new Date()}`))
+    .catch((err) => console.log(`ERROR at launch:`, err))
 
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
